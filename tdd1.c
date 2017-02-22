889cc6a0df29768bc5a7a5c51d2e67a55e9dbff4
차재민 <masterj@hansol.com>
2017-02-22 21:01:53 +0900

    tdd test file

---
+++ tdd1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+
+
+
+int minus(int a, int b) {
+  return a - b;
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+
+
+int main() {
+  int result = 0;
+  // insert
+
+
+
+  result = sum(5, 5);
+
+
+  // check
+  if (result == 5 ) {
+    printf("tdd check ok %d", result);
+
+  } else {
+    printf("tdd check failed %d", result);
+  }
+
+
+
+  return 0;
+}
