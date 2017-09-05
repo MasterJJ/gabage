7327fe35ce41f8636b3db9e88e2527ab1afa0e71
jeamin cha <jmcha@tellion.com>
2017-09-05 10:55:33 +0900

    add error file

---
+++ error.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+error
+
+int minus(int a, int b) {
+  return a - b;
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+
+
+int main() {
+  int result = 0;
+  // insert
+
+
+
+  result = sum(5, 5);
+
+
+  // check
+  if (result == 5 ) {
+    printf("tdd check ok %d", result);
+
+  } else {
+    printf("tdd check failed %d", result);
+  }
+
+
+
+  return 0;
+}
